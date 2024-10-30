0051e34337d2f4f9907cb2a441054a000acf0dae
takahashi-N <naotaka022@gmail.com>
2024-10-30 10:32:44 +0900

    add open modal with javascript

---
+++ app/javascript/controllers/top.js
@@ -0,0 +1,10 @@
+console.log('top.js');
+
+const exampleModalBtn = document.querySelector('#example_modal_btn');
+
+exampleModalBtn.addEventListener('click', function () {
+  console.log('exampleModalBtn click');
+  const exampleModal = new bootstrap.Modal('#example_modal');
+  exampleModal.show(exampleModal);
+})
+
