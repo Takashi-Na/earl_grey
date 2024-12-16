52d11658f6e24a65e7a1602c393df2a9da698db4
takahashi-N <naotaka022@gmail.com>
2024-12-16 16:41:15 +0900

    rename from custom to src

---
+++ app/javascript/controllers/src/top.js
@@ -0,0 +1,11 @@
+document.addEventListener('turbo:load', function () {
+  console.log('controllers/custom/top.js');
+
+  const exampleModalBtn = document.querySelector('#example_modal_btn');
+
+  exampleModalBtn.addEventListener('click', function () {
+    console.log('exampleModalBtn click');
+    const exampleModal = new bootstrap.Modal('#example_modal');
+    exampleModal.show(exampleModal);
+  })
+})
